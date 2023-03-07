6494ccebed1d1281515b164a2255157dcb428324
Esolder <40249820+Esolder@users.noreply.github.com>
2023-03-07 13:20:43 +0300

    Add places model

--- places/models.py
+++ places/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class Place(models.Model):
+    title = models.CharField('Название места', max_length=200)
+    description_short = models.CharField('Краткое описание', max_length=300)
+    description_long = models.TextField('Подробное описание')
+    longitude = models.FloatField('Долгота')
+    latitude = models.FloatField('Широта')
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
+    def __str__(self):
+        return self.title  
